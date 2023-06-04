1338b803e3558e93a247c77d934d30bba11a4ec9
root <root@ip-172-31-87-236.ec2.internal>
2023-06-04 05:52:22 +0000

    first ver script

---
+++ automation.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+sudo apt update -y
+
+REQUIRED_PKG=“apache2”
+
+PKG_OK=$(dpkg-query -W -f='${Status} ${Version}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG
+fi
+
+sudo systemctl enable apache2
+
+timestamp=$(date '+%d%m%Y-%H%M%S')
+
+zip /tmp/pirai-httpd-${timestamp}.tar.gz /var/log/apache2/access.log
+
+aws s3 cp /tmp/pirai-httpd-${timestamp}tar.gz s3://upgrad-piraisoodan
